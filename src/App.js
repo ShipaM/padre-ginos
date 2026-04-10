489b84edec6f81ec1fa781893002512e47ef94db
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2026-04-10 14:26:35 +0300

    feat (Vanila React Components)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.title),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-  );
+    React.createElement(Pizza, {
+      title: "Margherita",
+      description: "Tomato sauce, mozzarella, basil",
+    }),
+    React.createElement(Pizza, {
+      title: "Pepperoni",
+      description: "Tomato sauce, mozzarella, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      title: "Hawaiian",
+      description: "Tomato sauce, mozzarella, ham, pineapple",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
